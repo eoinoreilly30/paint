b360711c51fede2a8fd8ea2f8871cdcea891be13
eoin <eoinoreilly30@gmail.com>
2022-03-04 01:01:53 +0000

    Get bitmap from API

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const fs = require('fs')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
 
-try {
-    if (!fs.existsSync('bitmap.txt')) createBitmap()
-} catch (err) {
-    console.error(err)
-    return
-}
+app.use(cors())
+
+createBitmapIfNotExists()
 
 app.get('/', (req, res) => {
     res.send('This is the API for Grid Paint')
@@ -25,26 +23,33 @@ app.get('/grid', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Started on port ${port}`)
 })
 
 
-function createBitmap() {
-    let bitmap = []
-    let first_array = []
-    const [x_dims, y_dims] = [96, 54]
-
-    for (let y = 0; y < y_dims; y++) {
-        first_array.push(false)
-    }
-
-    for (let x = 0; x < x_dims; x++) {
-        bitmap.push([...first_array])
-    }
-
+function createBitmapIfNotExists() {
     try {
-        fs.writeFileSync('bitmap.txt', JSON.stringify(bitmap), 'utf8')
+        if (!fs.existsSync('bitmap.txt')) {
+            let bitmap = []
+            let first_array = []
+            const [x_dims, y_dims] = [96, 54]
+
+            for (let y = 0; y < y_dims; y++) {
+                first_array.push(false)
+            }
+
+            for (let x = 0; x < x_dims; x++) {
+                bitmap.push([...first_array])
+            }
+
+            try {
+                fs.writeFileSync('bitmap.txt', JSON.stringify(bitmap), 'utf8')
+            } catch (err) {
+                console.error(err)
+            }
+        }
     } catch (err) {
         console.error(err)
+        process.exit()
     }
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,47 +1,28 @@
+const axios = require('axios');
+
 if (module.hot) module.hot.accept()
 
 const DEBUG = false
+const BASE_URL = "http://localhost:3000"
 
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
 // TODO: screen size change listener
 
-let bitmap = []
-let first_array = []
-
 const width = window.innerWidth
 const height = window.innerHeight
 
-canvas.width  = width
+canvas.width = width
 canvas.height = height
 
-const x_dims = 96
-const y_dims = 54
-const x_increment = width / x_dims
-const y_increment = height / y_dims
+let bitmap = []
+let x_dims = 0
+let y_dims = 0
+let x_increment = 0
+let y_increment = 0
 
-for (let y = 0; y < y_dims; y++) {
-    if (DEBUG) {
-        context.moveTo(0, y*y_increment);
-        context.lineTo(width, y*y_increment);
-    }
-    first_array.push(false)
-}
-
-for (let x = 0; x < x_dims; x++) {
-    if (DEBUG) {
-        context.moveTo(x*x_increment, 0);
-        context.lineTo(x*x_increment, height);
-    }
-    bitmap.push([...first_array])
-}
-
-if (DEBUG) {
-    context.strokeStyle = "lightgrey";
-    context.lineWidth = 1
-    context.stroke();
-}
+getGrid()
 
 canvas.addEventListener("click", event => {
     let x = Math.floor(event.clientX / x_increment)
@@ -49,9 +30,50 @@ canvas.addEventListener("click", event => {
 
     if (bitmap[x][y]) {
         bitmap[x][y] = false
-        context.clearRect(x*x_increment, y*y_increment, x_increment, y_increment)
+        context.clearRect(x * x_increment, y * y_increment, x_increment, y_increment)
     } else {
         bitmap[x][y] = true
-        context.fillRect(x*x_increment, y*y_increment, x_increment, y_increment)
+        context.fillRect(x * x_increment, y * y_increment, x_increment, y_increment)
     }
 })
+
+function getGrid() {
+    axios.get(BASE_URL + '/grid')
+        .then((response) => {
+            bitmap = response.data
+            x_dims = bitmap.length
+            y_dims = bitmap[0].length
+            x_increment = width / x_dims
+            y_increment = height / y_dims
+            drawGrid()
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+function drawGrid() {
+    for (let x = 0; x < bitmap.length; x++) {
+        for (let y = 0; y < bitmap[0].length; y++) {
+            if (bitmap[x][y]) {
+                context.fillRect(x * x_increment, y * y_increment, x_increment, y_increment)
+            }
+        }
+    }
+
+    if (DEBUG) {
+        for (let y = 0; y < y_dims; y++) {
+            context.moveTo(0, y * y_increment);
+            context.lineTo(width, y * y_increment);
+        }
+
+        for (let x = 0; x < x_dims; x++) {
+            context.moveTo(x * x_increment, 0);
+            context.lineTo(x * x_increment, height);
+        }
+
+        context.strokeStyle = "lightgrey";
+        context.lineWidth = 1
+        context.stroke();
+    }
+}
